44370beea0d1303c0f76796c075bf4630f9c4f30
karanp0119 <78327706+karanp0119@users.noreply.github.com>
2021-03-08 01:08:02 -0500

    Add files via upload

---
+++ Hw.py
@@ -0,0 +1,19 @@
+import itertools
+
+
+def main():
+    print("This is demonstrating infinite iterators cycle and repeat")
+    t = [52, 54, 58, 63, 70, 75, 65]  # average temp in tampa last week
+    count = 0
+    for n in itertools.cycle(t):
+        if count > 10:
+            break
+        else:
+            print(n, end=" ")
+            count += 1
+    print()
+    print(list(itertools.repeat(t, 3)))
+
+
+if __name__ == "__main__":
+    main()
